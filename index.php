457f40cf1acab7647e7985811bd3d9cacf02d312
Usman <usmanyap0301@gmail.com>
2022-10-27 15:23:11 +0700

    menambahkan logout.php

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-<!DOCTYPE html>
+<?php
+include'koneksi.php';
+
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Binotify</title>
+
 </head>
 <body>
     <div class="login-user">
@@ -14,7 +19,7 @@
         </div>
         <div class="login-user__info">
             <p>you must login to next the music </p>
-            <a href="./auth/login.php" class="login">Login</a>
+            <a href="login.php" class="login">Login</a>
             <a href="./auth/signup.php"class="sign up">Miku : 'Haven't create an account yet?'</a>
             <div class="close">+</div>
         </div>
@@ -26,7 +31,7 @@
             <!-- End sidebar -->
 
             <!-- Music UI -->
-            <div class="musiccontainer"            id="home"><?php include("./pages/homeContent.php"); ?>
+            <div class="musiccontainer"id="home"><?php include("./pages/homeContent.php"); ?>
             </div>
 
             <div class="musiccontainer hide" id="favorites"><?php if ($authenticated) : ?>
@@ -42,3 +47,4 @@
     
 </body>
 </html>
+
--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $server = "localhost";
 $user = "root";
 $password ="";
-$nama_database = "tubesweb";
+$nama_database = "";
 
 $db = mysqli_connect($server, $user, $password,$nama_database);
 if (!$db){
