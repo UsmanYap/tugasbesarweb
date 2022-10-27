a8cfa3bdaeb48b95c8372ba03fa9e3a6cab2c106
FebriandaPutra <febriandaputra11@gmail.com>
2022-10-28 01:59:07 +0700

    Update koneksi.php
    perubahan nama variabel pada koneksi.php

--- utils/koneksi.php
+++ utils/koneksi.php
@@ -4,8 +4,8 @@ $user = "root";
 $password ="";
 $nama_database = "tubesweb";
 
-$db = mysqli_connect($server, $user, $password,$nama_database);
-if (!$db){
+$conn = mysqli_connect($server, $user, $password,$nama_database);
+if (!$conn){
     die("gagal terhubung dengan database : " . mysqli_connect_error());
 }
 
