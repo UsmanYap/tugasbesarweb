6fb5301830078b7d3be28b7c28a935b4523dcf60
FebriandaPutra <febriandaputra11@gmail.com>
2022-10-27 16:56:45 +0700

    menambahkan autentifikasi.php

---
+++ autentikasi.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+$uid = $name = '';
+$username = 'Guest';
+$authentucated = $admin = false;
+if (isset($_SESSION['id'])){
+    $uid = $_SESSION['username'];
+    $username = $_SESSION['admin'];
+    $admin = $_SESSION['name'];
+    $authenticated = true;
+}
+adfgdfggfd
