ac5e07686c746750ee8f57944667bab1c56d5a63
FebriandaPutra <febriandaputra11@gmail.com>
2022-10-28 02:48:43 +0700

    Update adminDashboard.php
    menambahkan link ke ./css/editSong.css

--- auth/adminDashboard.php
+++ auth/adminDashboard.php
@@ -18,7 +18,7 @@ if (!$authenticated) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
-    
+    <link rel="stylesheet" href="./css/editSong.css">
 </head>
 
 <body>
